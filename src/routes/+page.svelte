b42aca2d39f3ae4def24a69bfce7034a04445ef5
sudhir-b <sud.balaji@gmail.com>
2022-10-19 22:30:51 +0100

    Have some options

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Mayournaise</h1>
+<p>A silly project by Sudhir</p>
+
+<br />
+
+<form method="POST" >
+	<label for="cars">Choose an oil:</label>
+	<select name="oil" id="oil">
+		<option value="peanut">Peanut</option>
+		<option value="sunflower">Sunflower</option>
+		<option value="rapeseed">Rapeseed</option>
+		<option value="olive">Olive</option>
+	</select>
+
+	<br />
+	<br />
+
+	<label for="eggs">Choose egg:</label>
+	<select name="egg" id="egg">
+		<option value="chicken_regular">Chicken (regular)</option>
+		<option value="chicken_fancy">Chicken (fancy)</option>
+		<option value="duck">Duck</option>
+	</select>
+
+	<br />
+	<br />
+
+	<label for="acids">Choose an acid:</label>
+	<select name="acid" id="acid">
+		<option value="lemon_juice">Lemon juice</option>
+		<option value="lime_juice">Lime juice</option>
+		<option value="white_vinegar">White vinegar</option>
+		<option value="malt_vinegar">Malt vinegar</option>
+	</select>
+
+	<br />
+	<br />
+
+	<label for="mustards">Choose a mustard:</label>
+	<select name="mustard" id="mustard">
+		<option value="dijon">Dijon</option>
+		<option value="english">English</option>
+		<option value="whole_grain">Whole grain</option>
+	</select>
+
+	<br />
+	<br />
+
+	<button>Buy!</button>
+</form>
